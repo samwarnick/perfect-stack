e568c35835dbb08c0995ee301ee3c46d6d188ea5
Sam Warnick <sam@warnick.me>
2024-08-03 08:47:50 -0400

    Switch to local asset files

--- src/layout.tsx
+++ src/layout.tsx
@@ -7,16 +7,10 @@ export const Layout: FC = (props) => {
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>The Perfect Stack</title>
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-				/>
+				<link rel="stylesheet" href="/assets/picocss@2.0.6.min.css" />
 				<link rel="stylesheet" href="/assets/styles.css" />
-				<script src="https://unpkg.com/htmx.org@2.0.1/dist/htmx.min.js" />
-				<script
-					defer
-					src="https://cdn.jsdelivr.net/npm/alpinejs@3/dist/cdn.min.js"
-				/>
+				<script src="/assets/htmx@2.0.1.min.js" />
+				<script src="/assets/alpinejs@3.14.1.min.js" />
 			</head>
 			<body>
 				<main class="container">{props.children}</main>
